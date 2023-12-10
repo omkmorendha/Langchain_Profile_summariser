c3b2e1c232615313e3840a252acb3f58de70dd72
Om Morendha <omkmorendha@gmail.com>
2023-12-10 12:52:27 +0530

    Update for deployment

--- app.py
+++ app.py
@@ -1,6 +1,159 @@
-from flask import Flask, render_template, request, jsonify
+import os
+import requests
 
-from ice_breaker import ice_break
+from langchain.agents import initialize_agent, Tool, AgentType
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import PromptTemplate
+from langchain.utilities import SerpAPIWrapper
+
+from pydantic import BaseModel, Field
+from flask import Flask, render_template, request, jsonify
+from typing import Tuple, List
+
+class CustomSerpAPIWrapper(SerpAPIWrapper):
+    def __init__(self):
+        super(CustomSerpAPIWrapper, self).__init__()
+
+    @staticmethod
+    def _process_response(res: dict) -> str:
+        """Process response from SerpAPI."""
+        if "error" in res.keys():
+            raise ValueError(f"Got error from SerpAPI: {res['error']}")
+        if "answer_box" in res.keys() and "answer" in res["answer_box"].keys():
+            toret = res["answer_box"]["answer"]
+        elif "answer_box" in res.keys() and "snippet" in res["answer_box"].keys():
+            toret = res["answer_box"]["snippet"]
+        elif (
+            "answer_box" in res.keys()
+            and "snippet_highlighted_words" in res["answer_box"].keys()
+        ):
+            toret = res["answer_box"]["snippet_highlighted_words"][0]
+        elif (
+            "sports_results" in res.keys()
+            and "game_spotlight" in res["sports_results"].keys()
+        ):
+            toret = res["sports_results"]["game_spotlight"]
+        elif (
+            "knowledge_graph" in res.keys()
+            and "description" in res["knowledge_graph"].keys()
+        ):
+            toret = res["knowledge_graph"]["description"]
+        elif "snippet" in res["organic_results"][0].keys():
+            toret = res["organic_results"][0]["link"]
+
+        else:
+            toret = "No good search result found"
+        return toret
+
+
+def get_profile_url(name: str):
+    """Searches for Linkedin or twitter Profile Page."""
+    search = CustomSerpAPIWrapper()
+    res = search.run(f"{name}")
+    return res
+
+def scrape_linkedin_profile(linkedin_profile_url: str):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+
+    response = requests.get(
+        api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
+    )
+
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
+class PersonIntel(BaseModel):
+    summary: str = Field(description="Summary of the person")
+    facts: List[str] = Field(description="Interesting facts about the person")
+    topics_of_interest: List[str] = Field(
+        description="Topics that may interest the person"
+    )
+    ice_breakers: List[str] = Field(
+        description="Create ice breakers to open a conversation with the person"
+    )
+
+    def to_dict(self):
+        return {
+            "summary": self.summary,
+            "facts": self.facts,
+            "topics_of_interest": self.topics_of_interest,
+            "ice_breakers": self.ice_breakers,
+        }
+
+
+person_intel_parser: PydanticOutputParser = PydanticOutputParser(
+    pydantic_object=PersonIntel
+)
+
+def linkedin_lookup_agent(name: str) -> str:
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    template = """given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
+                          Your answer should contain only a URL"""
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google 4 linkedin profile page",
+            func=get_profile_url,
+            description="useful for when you need get the Linkedin Page URL",
+        )
+    ]
+
+    agent = initialize_agent(
+        tools=tools_for_agent,
+        llm=llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+    )
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+
+    linked_profile_url = agent.run(prompt_template.format_prompt(name_of_person=name))
+    return linked_profile_url
+
+def ice_break(name: str) -> Tuple[PersonIntel, str]:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+
+    summary_template = """
+         given the Linkedin information {linkedin_information} about a person from I want you to create:
+         1. a short summary
+         2. two interesting facts about them
+         3. A topic that may interest them
+         4. 2 creative Ice breakers to open a conversation with them
+        \n{format_instructions}
+     """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["linkedin_information", "twitter_information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
+    )
+
+    llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    result = chain.run(linkedin_information=linkedin_data)
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
 
 app = Flask(__name__)
 
@@ -27,4 +180,4 @@ def process():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=8080)
+    app.run(host="0.0.0.0", debug=False, port=5000)
